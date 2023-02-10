d9adaf7e3c1d74493542953ed0b8d15ed4812aed
hyesung <k_hyesung6@naver.com>
2023-02-10 17:44:38 +0900

    setting for AWS RDS(Mysql) and creating models, views

---
+++ shop/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .views import Category, Product
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', ]
+    prepopulated_fields = {'slug':('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'category', 'price', 
+    'stock', 'available_display', 'available_order', 'created', 'updated']
+    list_filter = ['available_display', 'created', 'updated', 'category']
+    prepopulated_fields = {'slug':('name',)}
+    list_editable = ['price', 'stock', 'available_display', 'available_order']
